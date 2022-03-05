34bacc1cebf4723cc5c925dab2e3a3a3ef23850c
ArturAlvaro <artur.alvaro73@gmail.com>
2022-03-05 10:15:16 -0300

    refactor: change seeders

--- src/seeders/20200812183211-Users.js
+++ src/seeders/20200812183211-Users.js
@@ -6,14 +6,14 @@ module.exports = {
         displayName: 'Amy Hamilton',
         email: 'amyhamilton@gmail.com',
         password: '123456',
-        image: 'https://cdn-icons.flaticon.com/png/512/706/premium/706807.png?token=exp=1646319495~hmac=c38685f6ed6d97e2b95340a29281870e',
+        image: 'http://localhost:3000/images/woman.png',
       },
       {
         id: 2,
         displayName: 'Michael Schopenhauer',
         email: 'michaelschopenhauer@gmail.com',
         password: '123456',
-        image: 'https://cdn-icons.flaticon.com/png/512/706/premium/706807.png?token=exp=1646319495~hmac=c38685f6ed6d97e2b95340a29281870e',
+        image: 'http://localhost:3000/images/man.png',
       },
       ], { timestamps: false });
   },
